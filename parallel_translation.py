d5ab55e808effa2d261e9fab99be06ba293812f1
rupy <rupyapps@gmail.com>
2015-04-09 11:49:15 +0900

    added line_wakati method

--- parallel_translation.py
+++ parallel_translation.py
@@ -44,7 +44,7 @@ class ParallelTranslation():
             with open(output_jp, 'w') as f:
                 f.write(row[1])
 
-    def wakachi(self):
+    def wakati(self):
 
         # directories existence check
         if not os.path.isdir('wakati'):
@@ -52,20 +52,40 @@ class ParallelTranslation():
 
         files = os.listdir('japanese')
         for file in files:
-            with open('japanese/' + file) as f:
-                txt = f.read()
+            with open('japanese/' + file) as f1:
+                txt = f1.read()
 
                 tagger = MeCab.Tagger("-Owakati")
                 wakati_result = tagger.parse(txt)
 
                 output_wak = urljoin('wakati/', file)
                 print "Saving: %s" % output_wak
-                with open(output_wak, 'w') as f:
-                    f.write(wakati_result)
+                with open(output_wak, 'w') as f2:
+                    f2.write(wakati_result)
+
+    def line_wakati(self):
+
+        # directories existence check
+        if not os.path.isdir('line_wakati'):
+            os.mkdir('line_wakati')
+
+        files = os.listdir('japanese')
+        for file in files:
+            with open('japanese/' + file) as f1:
+                output_wak = urljoin('line_wakati/', file)
+                with open(output_wak, 'w') as f2:
+                    for txt in f1.readlines():
+
+                        tagger = MeCab.Tagger("-Owakati")
+                        wakati_result = tagger.parse(txt)
+
+                        print "Saving: %s" % output_wak
+                        f2.write(wakati_result)
 
 if __name__=="__main__":
 
     csv_file = 'parallel_translation/pascal_sentence_numbers.csv'
     ps = ParallelTranslation(csv_file)
     # ps.read_csv_and_save_as_txt()
-    ps.wakachi()
+    # ps.wakati()
+    ps.line_wakati()
